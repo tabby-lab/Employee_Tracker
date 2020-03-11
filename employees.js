2fbd14d79703f5a92aa18c39840b41490333f4c9
Tabby Garcia <tabby.garcia1@gmail.com>
2020-03-11 13:23:29 -0500

    added departments but for some reason it is duplicating

--- employees.js
+++ employees.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
 let titleList = [];
-let managerList=[];
+let managerList = [];
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -24,28 +24,25 @@ function start() {
             name: "options",
             type: "list",
             message: "What would you like to do?",
-            choices: ["View All Employees", "View All Employees by Department", "View All Roles","View All Employees by Manager", 
-                "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+            choices: ["View All Employees", "View All Departments", "View All Roles", "View All Employees by Manager",
+                "Add Employee", "Add Role", "Add Department"]
         })
-        //need a then function and else if/else
-        //replace with switch
+
         .then(function (answer) {
             if (answer.options === "View All Employees") {
                 viewAllEmployee();
-            } else if (answer.options === "View All Employees by Department") {
-                viewAllEmployeesByDepartment();
-            }else if (answer.options === "View All Roles") {
+            } else if (answer.options === "View All Departments") {
+                viewAllDepartments();
+            } else if (answer.options === "View All Roles") {
                 viewAllRoles();
             } else if (answer.options === "View All Employees by Manager") {
                 viewAllEmployeesByManager();
             } else if (answer.options === "Add Employee") {
                 addEmployee();
-            } else if (answer.options === "Remove Employee") {
-                removeEmployee();
-            } else if (answer.options === "Update Employee Role") {
-                updateEmployeeRole();
-            } else if (answer.options === "Update Employee Manager") {
-                updateEmployeeManager();
+            } else if (answer.options === "Add Role") {
+                addRole();
+            } else if (answer.options === "Add Department") {
+                addDepartment();
             }
 
         });
@@ -66,26 +63,22 @@ LEFT JOIN department on department.id=role.department_id
     start();
 }
 //VIEW EMPLOYEE BY DEPT///////////////////////////////////////////////////////////////////////////
-function viewAllEmployeesByDepartment() {
-    connection.query(`SELECT employee.id,employee.first_name,employee.last_name 
-,role.title,role.salary ,department.name department,
-concat(employee2.first_name," ", employee2.last_name) manager
-FROM employee 
-LEFT JOIN role on employee.role_id=role.id
-LEFT JOIN employee employee2 on employee.manager_id=employee2.id
-LEFT JOIN department on department.id=role.department_id order by department`, function (err, data) {
-        console.table(data)
+function viewAllDepartments() {
+    connection.query("SELECT name FROM department", function (err, res) {
+        if (err) throw err;
+        console.table(res)
     })
     start();
 }
 
 //VIEW ROLES///////////////////////////////////////////////////////////
-function viewAllRoles(){
-connection.query("SELECT title FROM role", function(err,res){
-    if (err) throw err;
-    console.table(res);
+function viewAllRoles() {
+    connection.query("SELECT title FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+
+    })
     start();
-})
 }
 
 //VIEW EMPLOYEE BY MANAGER///////////////////////////////////////////////////////////////////////
@@ -96,43 +89,19 @@ function viewAllEmployeesByManager() {
     });
 
 }
-
-
-//needs an array of employees then put into "1"
-function removeEmployee() {
-    connection.query('DELETE FROM employee WHERE  = 1', (err, result) => {
-        if (err) throw err;
-
-        console.log(`Deleted ${result.affectedRows} row(s)`);
-    });
-    //maybe use this?
-    //app.delete('/employee/:id',(req,res)=>{
-    //connection.query("SELECT * FROM employee WHERE employee_id")
-    //}
-}
-// "UPDATE role SET new role/////////////////////////////////////////////////////
-function updateEmployeeRole() {
-    connection.query("UPDATE role_id SET title = '' WHERE id = 2");
-
-}
-// "UPDATE employee MANAGER/////////////////////////////////////////////////////////
-function updateEmployeeManager() {
-
-}
 // ADD EMPLOYEE////////////////////////////////////////////////////////////////////////
-
-function managerOption(){
-    return new Promise((resolve,reject)=>{
-        connection.query("SELECT * FROM employee WHERE manager_id IS NOT NULL", function(err,data){
+function managerOption() {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM employee WHERE manager_id IS NOT NULL", function (err, data) {
             if (err) throw err;
             resolve(data);
         });
     });
 }
 
-function roleOption(){
-    return new Promise((resolve,reject)=>{
-        connection.query("SELECT * FROM role", function(err,data){
+function roleOption() {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM role", function (err, data) {
             if (err) throw err;
             resolve(data);
         });
@@ -140,7 +109,7 @@ function roleOption(){
 }
 
 function addEmployee() {
-   
+
     roleOption().then(function (titles) {
         titleList = titles.map(role => role.title);
 
@@ -179,10 +148,10 @@ function addEmployee() {
                     const mySelectedManager = manager.find(item => (item.first_name + " " + item.last_name) === input.manager);
                     console.log(mySelectedManager.manager_id)
                     connection.query(`INSERT INTO employee(first_name, last_name, role_id,manager_id) VALUES ("${input.firstName}", "${input.lastName}", ${mySeelectedRole.id}, "${mySelectedManager.id}")`,
-                    function (err,res){
-                        if (err) throw err;
-                    }
-                    
+                        function (err, res) {
+                            if (err) throw err;
+                        }
+
                     );
                     start();
 
@@ -190,3 +159,39 @@ function addEmployee() {
         });
     });
 }
+//ADD DEPARTMENT/////////////////////////////////////////////////////////
+function addDepartment(){
+    inquirer.prompt([{
+        name: "department",
+        type: "input",
+        message: "What department would you like to add?"
+    }])
+    .then(function(input){
+        connection.query(`INSERT INTO department (name) VALUES("${input.department}")`,function(err,res){
+            if (err) throw err;
+            console.table(viewAllDepartments());
+            start();
+        })
+    })
+}
+
+
+
+// "UPDATE role SET new role/////////////////////////////////////////////////////
+// function updateEmployeeRole() {
+//     connection.query("UPDATE role_id SET title = '' WHERE id = 2");
+// }
+
+
+//REMOVE BONUS
+// function removeEmployee() {
+//     connection.query('DELETE FROM employee WHERE  = 1', (err, result) => {
+//         if (err) throw err;
+
+//         console.log(`Deleted ${result.affectedRows} row(s)`);
+//     });
+    //maybe use this?
+    //app.delete('/employee/:id',(req,res)=>{
+    //connection.query("SELECT * FROM employee WHERE employee_id")
+    //}
+// }
