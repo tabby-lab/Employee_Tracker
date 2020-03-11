7407845b3b9b25f0b49b6739b6b3d380d65275e8
Tabby Garcia <tabby.garcia1@gmail.com>
2020-03-10 19:38:09 -0500

    Making a function to add an employee. Got view employees by dept. to render and view employees by manager to render.

--- employees.js
+++ employees.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
-const inquirer = require("inquirer")
-require("console.table")
+const inquirer = require("inquirer");
+require("console.table");
+let titleList = [];
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -24,17 +26,12 @@ function start() {
             choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager",
                 "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
         })
-
-
-
         //need a then function and else if/else
         //replace with switch
         .then(function (answer) {
             if (answer.options === "View All Employees") {
                 viewAllEmployee();
-                
-            }
-            else if (answer.options === "View All Employees by Department") {
+            } else if (answer.options === "View All Employees by Department") {
                 viewAllEmployeesByDepartment();
             } else if (answer.options === "View All Employees by Manager") {
                 viewAllEmployeesByManager();
@@ -47,13 +44,12 @@ function start() {
             } else if (answer.options === "Update Employee Manager") {
                 updateEmployeeManager();
             }
-           
+
         });
 }
 //wtf
 function viewAllEmployee() {
-    connection.query(`
-SELECT employee.id,employee.first_name,employee.last_name 
+    connection.query(`SELECT employee.id,employee.first_name,employee.last_name 
 ,role.title,role.salary ,department.name department,
 concat(employee2.first_name," ", employee2.last_name) manager
 FROM employee 
@@ -63,38 +59,92 @@ LEFT JOIN department on department.id=role.department_id
 `, function (err, data) {
         console.table(data)
 
-       //// start()
-        
     })
-
-
 }
 
 function viewAllEmployeesByDepartment() {
-connection.query(`SELECT employee.id,employee.first_name,employee.last_name 
+    connection.query(`SELECT employee.id,employee.first_name,employee.last_name 
 ,role.title,role.salary ,department.name department,
 concat(employee2.first_name," ", employee2.last_name) manager
 FROM employee 
 LEFT JOIN role on employee.role_id=role.id
 LEFT JOIN employee employee2 on employee.manager_id=employee2.id
-LEFT JOIN department on department.id=role.department_id order by department`,function(err,data){
-    console.table(data)
-})
+LEFT JOIN department on department.id=role.department_id order by department`, function (err, data) {
+        console.table(data)
+    })
 }
 
 function viewAllEmployeesByManager() {
+    connection.query("SELECT manager_id,first_name,last_name FROM employee", function (err, res) {
+        if (err) throw (err);
+        console.table(res);
+    });
 
 }
+
 function addEmployee() {
 
 }
+
 function removeEmployee() {
+    connection.query('DELETE FROM employee WHERE employee_id = 1', (err, result) => {
+        if (err) throw err;
 
+        console.log(`Deleted ${result.affectedRows} row(s)`);
+    });
+    //maybe use this?
+    //app.delete('/employee/:id',(req,res)=>{
+    //connection.query("SELECT * FROM employee WHERE employee_id")
+    //}
 }
-
+// "UPDATE role SET new role
 function updateEmployeeRole() {
+    connection.query("UPDATE role_id SET title = '' WHERE id = 2");
 
 }
+// "UPDATE employee SET new employee
 function updateEmployeeManager() {
 
+}
+// ADD EMPLOYEE////////////////////////
+function addEmployee() {
+    let managerList=[];
+    roleOption().then(function (titles) {
+        titleList = titles.map(role => role.Title);
+
+        managerOption().then(function (managers) {
+            managerList = managers.map(manager => manager.manager);
+
+            inquirer.prompt([{
+                name: "firstName",
+                type: "input",
+                message: "What is the employee's first name?"
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "What is the employee's last name?"
+
+            },
+            {
+                name: "role",
+                type: "list",
+                message: "What is the employee's role?",
+                choices: "titleList"
+            },
+            {
+                name: "manager",
+                type: "list",
+                message: "Who is the employee manager?",
+                choices: managerList
+            }])
+                .then(function (input) {
+                    connection.query(`INSERT INTO employee(first_name, last_name, role_id,manager_id) VALUES ("${input.firstName}", "${input.lastName}", "${input.role}", "${input.manager}")`,
+                    function (err,res){
+                        if (err) throw err;
+                    }
+                    );
+                });
+        });
+    });
 }
